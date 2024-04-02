3f414915229b244d35ce3267e66d7b834673f9f5
ukrainian-bird84 <143319836+ukrainian-bird84@users.noreply.github.com>
2024-04-02 23:46:51 +0200

    iyg

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useEffect, useMemo, useState } from "react";
+import { nanoid } from "nanoid";
+import { NavLink, Route, Routes } from "react-router-dom";
+import clsx from "clsx";
+
+import MovieCast from './components/MovieCast/MovieCast';
+import MovieReviews from './components/MovieReviews/MovieReviews';
+
+import HomePage from './pages/HomePage';
+import MovieDetailsPage from './pages/MovieDetailsPage';
+import MoviesPage from './pages/MoviesPage';
+import NotFoundPage from './pages/NotFoundPage';
+
+function App() {
+  const Users = [users, setUsers];
+  
+}
+
+export default App;
